89e0a3e4e4fedd9bd8c777e60664025d49d67f69
Ben Webb <ben@salilab.org>
2017-10-20 15:31:19 -0700

    Improve test coverage of backend.

---
+++ test/backend/test_checker.py
@@ -0,0 +1,64 @@
+import unittest
+import mist
+import saliweb.test
+import saliweb.backend
+import os
+
+def make_input_file(contents):
+    d = saliweb.test.TempDir()
+    fname = os.path.join(d.tmpdir, 'input.txt')
+    with open(fname, 'w') as fh:
+        fh.write(contents)
+    return d, fname
+
+class Tests(saliweb.test.TestCase):
+    """Check InputFileCheck class"""
+
+    def test_bad_length(self):
+        """Test file with bad line lengths"""
+        d, fname = make_input_file("""#\t#\t#\tExps\tA1\tA2\tB1
+#\t#\t#\tBaits\tA\tB\tC
+Prey\t#\tLength\tBaitSims\tA\tA\tB|C
+Protein1\t#\t188\t#\t12\t4
+""")
+        check, msg = mist.InputFileCheck().fileCheck(fname)
+        self.assertFalse(check)
+        self.assertEqual(msg, '\n\tLine 4 disagrees with #Exp (7)!\n')
+
+    def test_bad_value(self):
+        """Test file with bad values"""
+        d, fname = make_input_file("""#\t#\t#\tExps\tA1\tA2\tB1
+#\t#\t#\tBaits\tA\tB\tC
+Prey\t#\tLength\tBaitSims\tA\tA\tB|C
+Protein1\t#\t188\t#\tFOO\tBAR\t27
+""")
+        check, msg = mist.InputFileCheck().fileCheck(fname)
+        self.assertFalse(check)
+        self.assertEqual(msg, '\n\tElement in line 4, row 5 is not a value!\n'
+                              '\tElement in line 4, row 6 is not a value!\n')
+
+    def test_repeated_experiment(self):
+        """Test file with repeated experiment"""
+        d, fname = make_input_file("""#\t#\t#\tExps\tA1\tA2\tA1
+#\t#\t#\tBaits\tA\tB\tC
+Prey\t#\tLength\tBaitSims\tA\tA\tB|C
+Protein1\t#\t188\t#\t12\t4\t27
+""")
+        check, msg = mist.InputFileCheck().fileCheck(fname)
+        self.assertFalse(check)
+        self.assertEqual(msg, '\n\tExperiment A1 repeated!\n')
+
+    def test_repeated_prey(self):
+        """Test file with repeated prey"""
+        d, fname = make_input_file("""#\t#\t#\tExps\tA1\tA2\tB1
+#\t#\t#\tBaits\tA\tB\tC
+Prey\t#\tLength\tBaitSims\tA\tA\tB|C
+Protein1\t#\t188\t#\t12\t4\t27
+Protein1\t#\t188\t#\t12\t4\t27
+""")
+        check, msg = mist.InputFileCheck().fileCheck(fname)
+        self.assertFalse(check)
+        self.assertEqual(msg, '\n\tPrey Protein1 repeated!\n')
+
+if __name__ == '__main__':
+    unittest.main()
--- test/backend/test_job.py
+++ test/backend/test_job.py
@@ -14,10 +14,55 @@ class JobTests(saliweb.test.TestCase):
         with open('param.txt', 'w') as fh:
             fh.write('training\nno_filtering\n')
         with open('input.txt', 'w') as fh:
-            fh.write('SAV_STRAV;;2378;;3782;3990;2294;842;2739;1533;2938;1806;1686\n')
+            fh.write('#\t#\t#\tExps\tA1\tA2\tB1\n')
+            fh.write('#\t#\t#\tBaits\tA\tB\tC\n')
+            fh.write('Prey\t#\tLength\tBaitSims\tA\tA\tB|C\n')
+            fh.write('Protein1\t#\t188\t#\t12\t4\t12\n')
         cls = j._run_in_job_directory(j.run)
         self.assert_(isinstance(cls, saliweb.backend.SGERunner),
                      "SGERunner not returned")
+
+    def test_run_not_ok(self):
+        """Test run method with bad input file"""
+        j = self.make_test_job(mist.Job, 'RUNNING')
+        d = saliweb.test.RunInDir(j.directory)
+        with open('param.txt', 'w') as fh:
+            fh.write('training\nno_filtering\n')
+        with open('input.txt', 'w') as fh:
+            fh.write('#\t#\t#\tExps\tA1\tA2\tA1\n')
+            fh.write('#\t#\t#\tBaits\tA\tB\tC\n')
+            fh.write('Prey\t#\tLength\tBaitSims\tA\tA\tB|C\n')
+            fh.write('Protein1\t#\t188\t#\t12\t4\t12\n')
+        cls = j._run_in_job_directory(j.run)
+        self.assert_(isinstance(cls, saliweb.backend.DoNothingRunner),
+                     "DoNothingRunner not returned")
+
+    def test_postprocess_ok(self):
+        """Test postprocess method of OK run"""
+        j = self.make_test_job(mist.Job, 'RUNNING')
+        d = saliweb.test.RunInDir(j.directory)
+        with open('output.log', 'w') as fh:
+            fh.write('test header\n')
+        with open('output_metrics.out', 'w') as fh:
+            fh.write('Bait\tPrey\tReproducibility\tAbundance\tSpecificity\n')
+            fh.write('A\tB\ttestrep\ttestab\ttestsp\n')
+        with open('output_mist.out', 'w') as fh:
+            fh.write('Bait\tPrey\tMiST score\n')
+            fh.write('A\tB\ttestmist\n')
+        j.postprocess()
+        with open('MistOutput.txt') as fh:
+            contents = fh.read()
+        self.assertEqual(contents, """# test header
+
+#Bait\tPrey\tReproducibility\tAbundance\tSpecificity\tMiST
+A\tB\ttestrep\ttestab\ttestsp\ttestmist
+""")
+
+    def test_postprocess_bad(self):
+        """Test postprocess method of bad run"""
+        j = self.make_test_job(mist.Job, 'RUNNING')
+        d = saliweb.test.RunInDir(j.directory)
+        j.postprocess() # should do nothing
 
 if __name__ == '__main__':
     unittest.main()
